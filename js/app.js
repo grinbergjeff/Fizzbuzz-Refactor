92677469d9c61dd4dc5b2aeca277764c2514fe0f
Jeffrey Grinberg <grinbergjeff@gmail.com>
2015-06-19 22:30:51 -0400

    Implemented fizzbuzz function

--- js/app.js
+++ js/app.js
@@ -6,7 +6,30 @@ $(document).ready(function() {
 
 function refactfizzbuzz() {
 	var maxvalue  = newvalue();
-	
+	//Run Fizzbuzz with maxvalue
+		for ( var i = 1; i < maxvalue; i++) {
+		//Prioritize if conditions to check for divisible by 3 AND 5
+		if ( i % 3 === 0 && i % 5 === 0) {
+			console.log("fizz buzz");
+			$('body').append('<p>fizzbuzz</p>');
+			
+	}
+		//Check if divisible by 3
+		else if (i % 3 === 0) {
+			console.log('fizz');	
+			$('body').append('<p>fizz</p>');
+		}
+		//Check if divisble by 5
+		else if (i % 5 === 0) {
+			console.log('buzz');
+			$('body').append('<p>buzz</p>');
+		}
+		//Finally, since the number is not div by 3 or 5, just print number
+		else {
+			console.log(i);	
+			$('body').append('<p>' + i + '</p>');
+		}
+	}
 }
 
 function newvalue() {
@@ -17,9 +40,6 @@ function newvalue() {
 			$(this).val('');
 			//Return the value to overall fizzbuzz program
 			return valuenew;
-			
-			//Run Fizzbuzz with maxvalue
-			
 		}
-	});
-}
+	})
+};
